c0614b10860570f4e298ef13e47bd2621848684d
Kyle Ton <kyleton06@gmail.com>
2025-05-07 13:19:06 -0700

    finished code carnival lab 6

--- c++/CS179/CodeCarnivalProject/main.cpp
+++ c++/CS179/CodeCarnivalProject/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
-#include <vector> 
+#include <vector>
+#include <map>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -36,6 +39,8 @@ class Player
         string name;
         int tickets;
         LocationNode* locationPtr;
+        bool hasRope;
+        map<string, bool> techniciansUpset;  // Track which technicians are upset with the player
 
     public:
         Player() 
@@ -43,6 +48,7 @@ class Player
             name = "";
             tickets = 0;
             locationPtr = nullptr;
+            hasRope = false;
         }
 
         void setName(const string& name) 
@@ -78,6 +84,33 @@ class Player
         {
             return locationPtr;
         }
+
+        void pickUpRope() 
+        {
+            hasRope = true;
+            cout << "\nYou picked up a sturdy coil of rope. This might be useful later!\n";
+        }
+
+        bool hasRopeItem() const 
+        {
+            return hasRope;
+        }
+
+        void useRope() 
+        {
+            hasRope = false;
+        }
+
+        void upsetTechnician(const string& techName) 
+        {
+            techniciansUpset[techName] = true;
+        }
+
+        bool isTechnicianUpset(const string& techName) const 
+        {
+            auto it = techniciansUpset.find(techName);
+            return (it != techniciansUpset.end() && it->second);
+        }
 };
 
 struct Area : public LocationNode 
@@ -87,7 +120,6 @@ struct Area : public LocationNode
         locationName = name;
     }
 };
-
 
 // Booth Struct
 struct Booth 
@@ -173,6 +205,27 @@ struct GameBooth : public Booth
             int reward = rand() % 5 + 1; // Random ticket win
             player.addOrSubTickets(reward);
             cout << "\nYou played " << gameName << " and won " << reward << " tickets!\n";
+            
+            // Chance to meet a technician during the game
+            int meetChance = rand() % 10;
+            if (meetChance < 3) { // 30% chance to meet a technician
+                string techName;
+                if (gameName == "Ring Toss") {
+                    techName = "Rusty";
+                    cout << "\nYou bumped into Rusty, the Ferris Wheel technician. ";
+                } else {
+                    techName = "Sparky";
+                    cout << "\nYou met Sparky, the Roller Coaster technician. ";
+                }
+                
+                // 50% chance to upset the technician
+                if (rand() % 2 == 0) {
+                    cout << "Your rude behavior seemed to upset them!\n";
+                    player.upsetTechnician(techName);
+                } else {
+                    cout << "They seemed friendly and gave you some carnival tips.\n";
+                }
+            }
         } 
         else 
         {
@@ -185,21 +238,73 @@ struct RideBooth : public Booth
 {
     string rideName;
     int ridePrice;
+    bool isBroken;
+    string technicianName;
 
-    RideBooth(string name, int price, LocationNode* location) 
+    RideBooth(string name, int price, LocationNode* location, string techName) 
     {
         boothName = name;
         rideName = name;
         ridePrice = price;
-        locationPtr = location; 
+        locationPtr = location;
+        isBroken = false;
+        technicianName = techName;
     }
 
-    void ride(Player& player) 
+    void ride(Player& player, map<string, string>& technicianContacts) 
     {
         if (player.getTickets() >= ridePrice) 
         {
             player.addOrSubTickets(-ridePrice);
-            cout << "\nYou enjoyed the " << rideName << " ride!\n";
+            
+            // 25% chance the ride breaks down
+            if (rand() % 4 == 0) {
+                isBroken = true;
+                cout << "\nOh no! The " << rideName << " has broken down while you're on it!\n";
+                
+                if (rideName == "Ferris Wheel") {
+                    cout << "You're stuck at the very top of the Ferris Wheel!\n";
+                } else if (rideName == "Mini Roller Coaster") {
+                    cout << "You're stuck upside down on the loop!\n";
+                } else if (rideName == "Haunted House") {
+                    cout << "You're stuck in the darkest part of the Haunted House with strange noises all around you!\n";
+                }
+                
+                // Check if technician is available or upset
+                if (player.isTechnicianUpset(technicianName)) {
+                    cout << "\nYou call for " << technicianName << " using the emergency phone, but when they hear it's you, they refuse to come help!\n";
+                    
+                    if (rideName == "Ferris Wheel" && player.hasRopeItem()) {
+                        cout << "\nLuckily, you have that rope you picked up earlier! You use it to safely climb down from the Ferris Wheel.\n";
+                        player.useRope();
+                    } else if (rideName == "Haunted House") {
+                        cout << "\nYou'll have to find your own way out through the scary dark corridors...\n";
+                        // Lose some tickets in the process
+                        int lostTickets = rand() % 3 + 1;
+                        player.addOrSubTickets(-lostTickets);
+                        cout << "You lost " << lostTickets << " tickets in your panic to escape!\n";
+                    } else {
+                        cout << "\nAfter hanging upside down for what feels like hours, another carnival worker finally notices and helps you!\n";
+                        // Lose some tickets for the trouble
+                        player.addOrSubTickets(-2);
+                        cout << "You lost 2 tickets for the inconvenience.\n";
+                    }
+                } else {
+                    // Get technician's contact info and call for help
+                    string contact = technicianContacts[technicianName];
+                    cout << "\nYou check the emergency contact list and call " << technicianName << " at " << contact << ".\n";
+                    cout << "They arrive quickly and fix the " << rideName << ". You're back on solid ground!\n";
+                    
+                    // Bonus tickets for your trouble
+                    int bonus = rand() % 3 + 1;
+                    player.addOrSubTickets(bonus);
+                    cout << "For your patience, you receive " << bonus << " bonus tickets!\n";
+                }
+                
+                isBroken = false; // Ride is fixed now
+            } else {
+                cout << "\nYou enjoyed the " << rideName << " ride!\n";
+            }
         } 
         else 
         {
@@ -208,7 +313,6 @@ struct RideBooth : public Booth
     }
 };
 
-
 // Function to create and link locations
 vector<LocationNode*> createLocations() 
 {
@@ -216,7 +320,7 @@ vector<LocationNode*> createLocations()
 
     // Create locations
     locations.push_back(new LocationNode{"Exit"}); // index 0
-    locations.push_back((new LocationNode{"Ticket Booth"})); // index 1
+    locations.push_back(new LocationNode{"Ticket Booth"}); // index 1
     locations.push_back(new LocationNode{"Game Area"}); // index 2
     locations.push_back(new LocationNode{"Food Area"}); // index 3
     locations.push_back(new LocationNode{"Ring Toss"}); // index 4
@@ -226,19 +330,23 @@ vector<LocationNode*> createLocations()
     locations.push_back(new LocationNode{"Ride Area"}); // index 8
     locations.push_back(new LocationNode{"Ferris Wheel"}); // index 9
     locations.push_back(new LocationNode{"Mini Roller Coaster"}); // index 10
+    locations.push_back(new LocationNode{"Haunted House"}); // index 11
+    locations.push_back(new LocationNode{"Maintenance Shed"}); // index 12
     
 
     // Link locations together (ALL CAN EXIT)
     locations[1]->paths = {locations[2], locations[3], locations[8], locations[0]}; // Ticket Booth -> Game/Food/Ride Areas + Exit
-    locations[2]->paths = {locations[3], locations[4], locations[5], locations[0]}; // Game Area -> Food Area + Game Booths + Exit
-    locations[3]->paths = {locations[2], locations[6], locations[7], locations[0]}; // Food Area -> Game Area + Food Booths + Exit
+    locations[2]->paths = {locations[3], locations[4], locations[5], locations[8], locations[0]}; // Game Area -> Food Area + Game Booths + Ride Area + Exit
+    locations[3]->paths = {locations[2], locations[6], locations[7], locations[8], locations[0]}; // Food Area -> Game Area + Food Booths + Ride Area + Exit
     locations[4]->paths = {locations[2], locations[0]}; // Ring Toss -> Game Area + Exit
     locations[5]->paths = {locations[2], locations[0]}; // Archery Targets -> Game Area + Exit
     locations[6]->paths = {locations[3], locations[0]}; // Hot Dog Stand -> Food Area + Exit
     locations[7]->paths = {locations[3], locations[0]}; // Pizza Stand -> Food Area + Exit
-    locations[8]->paths = {locations[2], locations[3], locations[9], locations[10], locations[0]}; // Ride Area -> Food/Game Areas + Ride Booths + Exit
+    locations[8]->paths = {locations[2], locations[3], locations[9], locations[10], locations[11], locations[12], locations[0]}; // Ride Area -> Food/Game Areas + Ride Booths + Maintenance Shed + Exit
     locations[9]->paths = {locations[8], locations[0]}; // Ferris Wheel -> Ride Area + Exit 
     locations[10]->paths = {locations[8], locations[0]}; // Mini Roller Coaster -> Ride Area + Exit
+    locations[11]->paths = {locations[8], locations[0]}; // Haunted House -> Ride Area + Exit
+    locations[12]->paths = {locations[8], locations[0]}; // Maintenance Shed -> Ride Area + Exit
 
     return locations;
 }
@@ -253,6 +361,7 @@ void createBooths(vector<FoodBooth*>& foodBooths, vector<GameBooth*>& gameBooths
     LocationNode* pizzaStand = locations[7];
     LocationNode* ferrisWheel = locations[9];
     LocationNode* miniRollerCoaster = locations[10];
+    LocationNode* hauntedHouse = locations[11];
 
     // Create food booths 
     foodBooths.push_back(new FoodBooth("Hot Dog", 3, hotDogStand));
@@ -262,15 +371,34 @@ void createBooths(vector<FoodBooth*>& foodBooths, vector<GameBooth*>& gameBooths
     gameBooths.push_back(new GameBooth("Ring Toss", 2, ringToss));
     gameBooths.push_back(new GameBooth("Archery Targets", 4, archeryTargets));
 
-    rideBooths.push_back(new RideBooth("Ferris Wheel", 6, ferrisWheel));
-    rideBooths.push_back(new RideBooth("Mini Roller Coaster", 8, miniRollerCoaster));
+    // Create ride booths with the technician responsible for each ride
+    rideBooths.push_back(new RideBooth("Ferris Wheel", 6, ferrisWheel, "Rusty"));
+    rideBooths.push_back(new RideBooth("Mini Roller Coaster", 8, miniRollerCoaster, "Sparky"));
+    rideBooths.push_back(new RideBooth("Haunted House", 7, hauntedHouse, "Spooky"));
 }
 
+// Function to create emergency contacts map
+map<string, string> createTechnicianContacts() {
+    map<string, string> contacts;
+    contacts["Rusty"] = "111-0123";
+    contacts["Sparky"] = "222-4567";
+    contacts["Spooky"] = "333-8901";
+    return contacts;
+}
 
-
-void interactWithBooths(Player& player, vector<FoodBooth*>& foodBooths, vector<GameBooth*>& gameBooths, vector<RideBooth*>& rideBooths)
+void interactWithBooths(Player& player, vector<FoodBooth*>& foodBooths, vector<GameBooth*>& gameBooths, vector<RideBooth*>& rideBooths, map<string, string>& technicianContacts)
 {
     LocationNode* currentLocation = player.getLocation();
+
+    // Check if player is at the maintenance shed to get rope
+    if (currentLocation->locationName == "Maintenance Shed" && !player.hasRopeItem()) {
+        cout << "\nYou notice a coil of sturdy rope in the maintenance shed. It might be useful. Take it? (y/n)\n";
+        char choice;
+        cin >> choice;
+        if (choice == 'y') {
+            player.pickUpRope();
+        }
+    }
 
     // Check food booths
     for (FoodBooth* food : foodBooths) 
@@ -320,7 +448,7 @@ void interactWithBooths(Player& player, vector<FoodBooth*>& foodBooths, vector<G
             cin >> choice;
             if (choice == 'y') 
             {
-                ride->ride(player);  
+                ride->ride(player, technicianContacts);  
             }
             else
             {
@@ -331,7 +459,6 @@ void interactWithBooths(Player& player, vector<FoodBooth*>& foodBooths, vector<G
 
     cout << "You have " << player.getTickets() << " tickets left.\n";
 }
-
 
 // Function to move player
 void movePlayer(Player& player) 
@@ -344,7 +471,6 @@ void movePlayer(Player& player)
     {
         cout << i + 1 << ". " << currentLocation->paths[i]->locationName << endl;
     }
-
 
     int choice;
     cout << "\nEnter your choice: ";
@@ -366,10 +492,31 @@ void movePlayer(Player& player)
     }
 }
 
+// Function to display the technician contact list
+void showTechnicianContacts(const map<string, string>& technicianContacts) {
+    cout << "\n===== MAINTENANCE STAFF CONTACT LIST =====\n";
+    for (const auto& pair : technicianContacts) {
+        cout << pair.first << " (";
+        
+        if (pair.first == "Rusty") {
+            cout << "Ferris Wheel";
+        } else if (pair.first == "Sparky") {
+            cout << "Mini Roller Coaster";
+        } else if (pair.first == "Spooky") {
+            cout << "Haunted House";
+        }
+        
+        cout << " Technician): " << pair.second << endl;
+    }
+    cout << "======================================\n";
+}
 
 // Main function
 int main() 
 {
+    // Seed random number generator
+    srand(time(nullptr));
+    
     // Initialize player
     Player player;
     player.setName(startGame());
@@ -381,15 +528,25 @@ int main()
     vector<GameBooth*> gameBooths;
     vector<RideBooth*> rideBooths;
     createBooths(foodBooths, gameBooths, rideBooths, locations);
+    
+    // Create technician contact map
+    map<string, string> technicianContacts = createTechnicianContacts();
 
     player.setLocation(locations[1]); // Start at Ticket Booth
 
     cout << "You are currently at: " << player.getLocation()->locationName << endl;
+    cout << "\nTIP: There's a list of maintenance staff contacts posted around the carnival. It might be useful if a ride breaks down!\n";
 
     while (true)
     {
+        // If at the Ride Area, show the technician contacts list
+        if (player.getLocation()->locationName == "Ride Area") {
+            cout << "\nYou notice a posted sign with emergency contacts.\n";
+            showTechnicianContacts(technicianContacts);
+        }
+        
         movePlayer(player);
-        interactWithBooths(player, foodBooths, gameBooths, rideBooths);
+        interactWithBooths(player, foodBooths, gameBooths, rideBooths, technicianContacts);
     }
 
     // Deallocate memory
