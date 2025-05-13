408bf8d1d51a7f2823c11318e9ecf8e36e8e3376
Kyle Ton <kyleton06@gmail.com>
2025-05-12 19:16:26 -0700

    beginning proj7

---
+++ python/Project7/AVL_RedBlackTrees.py
@@ -0,0 +1,10 @@
+### Section 1 of Project 7
+
+## Part A
+# Start with empty tree, provide insertion order for original sequence that will result in NO ROTATIONS
+
+## Part B
+# Start with empty tree, provide insertion order for original sequence that will result in ONLY ONE ROTATION
+
+## Part C
+# What other inserts besides key 22 will cause a 3-nodde crossover?
